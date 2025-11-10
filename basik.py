a0fb96e47303d24481e782eef7294882d0b077e2
Cliff-D56 <cliffordgardner9@gmail.com>
2025-11-10 12:42:17 -0600

    first commit

---
+++ basik.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ") #This is a string
+age = 28 #This is an int
+List = ["Wrath","Edward","Rahim"] #This is a list
+List.append(name)
+print(name +  " aint your name, your name is Shithead!!!")
+print(List)
---
+++ first.py
@@ -0,0 +1,1 @@
+print("Hello Clifford")
