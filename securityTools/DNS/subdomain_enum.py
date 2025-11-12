791bf8a0dcfc993f77442d6d557963ee9f30e207
Cliff-D56 <cliffordgardner9@gmail.com>
2025-11-12 09:53:09 -0600

    Subdomain Enum Tool

---
+++ securityTools/DNS/subdomain_enum.py
@@ -0,0 +1,37 @@
+import requests
+import threading
+from tqdm import tqdm
+
+domain = 'google.com'
+
+with open('subdomains.txt') as file:
+    subdomains = file.read().splitlines()
+
+discovered_subdomains = []
+
+lock = threading.Lock()
+
+def check_sumdomains(subdomain):
+    url = f'http://{subdomain}.{domain}'
+    try:
+        requests.get(url)
+    except requests.ConnectionError:
+        pass
+    else: 
+        print("[+] Dicovered subdomain ",url)
+        with lock:
+            discovered_subdomains.append(url)
+
+threads = []
+
+for subdomain in subdomains:
+    thread = threading.Thread(target=check_sumdomains, args=(subdomain,))
+    thread.start()
+    threads.append(thread)
+
+for thread in threads:
+    thread.join()
+
+with open("discovered_subdomains.txt", 'w') as f:
+    for subdomain in discovered_subdomains:
+        print(subdomain, file=f)
