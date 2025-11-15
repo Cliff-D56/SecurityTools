e6707dede2a0a02655dc59fa6e7600400e6b5d24
Cliff-D56 <cliffordgardner9@gmail.com>
2025-11-15 15:44:23 -0600

    Trying to figure out how to stop duplicate results from decrypt_pdf function

--- securityTools/PDF/brute_force_cracker.py
+++ securityTools/PDF/brute_force_cracker.py
@@ -1,4 +1,5 @@
 import itertools
+import string
 import pikepdf
 import time
 from tqdm import tqdm
@@ -27,15 +28,56 @@ def try_password(pdf_file,password):
     except pikepdf._core.PasswordError:
         return None
     
-def decrypt_pdf(pdf_file, passwords, total_passwords, max_workers=4):
-    with tqdm(total=total_passwords, desc="Decrypting PDF", unit='password') as pbar:
-        with ThreadPoolExecutor(max_workers=max_workers) as executor:
-            future_to_passwords = {executor.submit(try_password,pdf_file,pwd):pwd for pwd in passwords}
+# def decrypt_pdf(pdf_file, passwords, total_passwords, max_workers=4):
+#     with tqdm(total=total_passwords, desc="Decrypting PDF", unit='password') as pbar:
+#         with ThreadPoolExecutor(max_workers=max_workers) as executor:
+#             future_to_password = {executor.submit(try_password,pdf_file,pwd):pwd for pwd in passwords}
 
-            for future in tqdm(future_to_passwords,total=total_passwords):
-                password = future_to_passwords[future]
-                if future.result():
-                    return future.result()
-                pbar.update(1)
+#             for future in tqdm(future_to_password,total=total_passwords):
+#                 password = future_to_password[future]
+#                 if future.result():
+#                     return future.result()
+#                 pbar.update(1)
+#     print("Unable to succeed")
+#     return None
+
+
+def decrypt_pdf(pdf_file, passwords, total_passwords, max_workers=4):
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures = {executor.submit(try_password, pdf_file, pwd): pwd for pwd in passwords}
+
+        for future in tqdm(as_completed(futures), total=total_passwords, desc="Decrypting PDF"):
+            result = future.result()
+            if result:
+                return result
     print("Unable to succeed")
     return None
+
+
+if __name__== '__main__':
+    
+    parser = argparse.ArgumentParser(description="Decrypt a password protected PDF file")
+    parser.add_argument("pdf_file",help="Path to password protected PDF file")
+    parser.add_argument("-w", "--wordlist", help="Path to password list file",default=None)
+    parser.add_argument("-g","--generate", action="store_true",help="Generate passwords")
+    parser.add_argument("-min", "--min-length",type=int, help="Minimum length of password to generate",default=1)
+    parser.add_argument("-max","--max-length",type=int,help="Max length of password",default=5)
+    parser.add_argument("-c","--charset",type=str, help="Characters in use for password generation",default=string.ascii_letters + string.digits + string.punctuation)
+    parser.add_argument("--max_workers",type=int, help="Max # of workers of threads", default=4)
+    args = parser.parse_args()
+
+    if args.generate:
+        passwords = generate_passwords(args.charset, args.min_length, args.max_length)
+        total_passwords = sum(1 for _ in generate_passwords(args.charset, args.min_length, args.max_length))
+    elif args.wordlist:
+        passwords = load_wordlist(args.wordlist)
+        total_passwords = sum(1 for _ in load_wordlist(args.wordlist))
+    else:
+        print("Either --wordlist must be provided or --generate must be specified")
+        exit(1)
+    decrypted_password = decrypt_pdf(args.pdf_file, passwords, total_passwords,args.max_workers)
+
+    if decrypted_password:
+        print(f"PDF Decrypted, password is: {decrypted_password}")
+    else:
+        print("Unsuccessful to decrypt PDF. Password not found")
